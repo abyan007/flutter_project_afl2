6568fdc99a7c64e2cc50c37c0df99d78cb70920e
“abyan007” <muhamadabyanmakasar@gmail.com>
2025-10-02 21:19:08 +0800

    mdc 102

--- lib/home.dart
+++ lib/home.dart
@@ -1,35 +1,52 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
+import 'model/products_repository.dart';
+import 'model/product.dart';
+import 'supplemental/product_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards (102)
-  // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
-    // TODO: Return an AsymmetricView (104)
-    // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+    return Scaffold(
+      // ✅ Top App Bar
+      appBar: AppBar(
+        title: const Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // TODO: Implement search (future step)
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.tune),
+            onPressed: () {
+              // TODO: Implement filter (future step)
+            },
+          ),
+        ],
       ),
-      // TODO: Set resizeToAvoidBottomInset (101)
+
+      // ✅ Grid Produk
+      body: GridView.count(
+        crossAxisCount: 2, // 2 kolom
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0, // ukuran card
+        children: _buildGridCards(context),
+      ),
     );
+  }
+
+  List<Widget> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    return products.map((product) {
+      return ProductCard(product: product);
+    }).toList();
   }
 }
